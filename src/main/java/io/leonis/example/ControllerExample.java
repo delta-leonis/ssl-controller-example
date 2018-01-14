fdfe709b3f3feee8a28a632487f04542e1396856
Jeroen de Jong <thumbnail95@gmail.com>
2018-01-14 21:35:10 +0100

    Allow arguments to change the multicast address
    defaults to localhost:1234. params can be passed using 'ip:123.123.123.123 port:1234' syntax.

--- src/main/java/io/leonis/example/ControllerExample.java
+++ src/main/java/io/leonis/example/ControllerExample.java
@@ -1,8 +1,8 @@
 package io.leonis.example;
 
-import io.leonis.subra.game.data.*;
-import io.leonis.subra.game.data.Player;
+import com.google.common.collect.*;
 import io.leonis.subra.game.data.Player.PlayerIdentity;
+import io.leonis.subra.game.data.*;
 import io.leonis.subra.ipc.network.StrategyMulticastSubscriber;
 import io.leonis.subra.ipc.peripheral.*;
 import io.leonis.subra.ipc.peripheral.JamepadController.JamepadControllerIdentity;
@@ -20,14 +20,29 @@ import reactor.core.publisher.Flux;
 /**
  * @author Jeroen de Jong
  */
-
 public class ControllerExample {
+  private static final Map<String, String> DEFAULTS = ImmutableMap.of(
+    "port", "1234",
+    "ip", "localhost");
   public static void main(final String[] args) throws IOException {
+    Map<String, String> input = Stream.of(args)
+        .map(arg -> arg.split(":"))
+        .collect(Collectors.toMap(strings -> strings[0], strings -> strings[1]));
+
+    Map<String, String> params = DEFAULTS.entrySet().stream().collect(Collectors.toMap(
+        Entry::getKey,
+        def -> input.getOrDefault(def.getKey(), def.getValue())));
+
+    new ControllerExample(params.get("ip"), Integer.parseInt(params.get("port")));
+  }
+
+  public ControllerExample(String ip, int port) throws IOException {
     // The mapping of controller number to player identity
-    final Map<JamepadControllerIdentity, Set<PlayerIdentity>> mapping = new HashMap<>();
-    mapping.put(new JamepadControllerIdentity(1), new HashSet<>(Arrays.asList(
-                                                    new PlayerIdentity(1, TeamColor.BLUE),
-                                                    new PlayerIdentity(2, TeamColor.BLUE))));
+    final Map<JamepadControllerIdentity, Set<PlayerIdentity>> mapping = ImmutableMap.of(
+        new JamepadControllerIdentity(1),
+        new HashSet<>(Arrays.asList(
+            new PlayerIdentity(1, TeamColor.BLUE),
+            new PlayerIdentity(2, TeamColor.BLUE))));
 
     final Function<JamepadController, PlayerCommand> handler = new JamepadControllerHandler();
 
@@ -40,8 +55,11 @@ public class ControllerExample {
                 Entry::getValue
             ))
         ).map(ControllerStrategySupplier::new)
-        .subscribe(new StrategyMulticastSubscriber<>(new MulticastSubscriber(InetAddress.getByName("localhost"), 1234)));
+        .subscribe(
+            new StrategyMulticastSubscriber<>(
+                new MulticastSubscriber(InetAddress.getByName(ip), port)));
   }
+
 
   @Value
   public static class ControllerStrategySupplier implements Strategy.Supplier {
@@ -50,7 +68,9 @@ public class ControllerExample {
     @Override
     public Map<PlayerIdentity, PlayerCommand> getStrategy() {
       return map.entrySet().stream().flatMap(
-          entry -> entry.getValue().stream().collect(Collectors.toMap(Function.identity(), id -> entry.getKey())).entrySet().stream()
+          entry -> entry.getValue().stream().collect(
+              Collectors.toMap(Function.identity(), id -> entry.getKey())
+          ).entrySet().stream()
       ).collect(Collectors.toMap(
           Entry::getKey,
           Entry::getValue
