07cce9a46a6b36bd36f57de2e2c513395a3a5b4e
Rimon Oz <rimooz@gmail.com>
2018-01-17 20:52:02 +0100

    add comment

--- src/main/java/io/leonis/example/ControllerExample.java
+++ src/main/java/io/leonis/example/ControllerExample.java
@@ -60,6 +60,7 @@ public class ControllerExample {
                         .map(identity ->
                             // of identities paired to commands
                             new SimpleImmutableEntry<>(identity, handler.apply(mapping.getKey()))))
+                // group the commands by pairs by identity
                 .collect(Collectors.groupingBy(Entry::getKey))
                 .entrySet().stream()
                 // compute the average command per identity
